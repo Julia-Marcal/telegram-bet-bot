b166d99d88dd0485ba84a882a6351290515a6d91
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-05-23 11:17:29 -0300

    adding list of options

--- src/services/default_list.ts
+++ src/services/default_list.ts
@@ -1,3 +1,25 @@
-export const allowed_leagues: string[] = [
+export const default_leagues: string[] = [
   'Brasileiro Série A',
 ]
+
+export const allowed_leagues: string[] = [
+  'Brasileiro Série A',
+  'Premier League',
+  'LaLiga',
+  'Serie A',
+  'Bundesliga',
+  'Ligue 1',
+  'Primeira Liga',
+  'Eredivisie',
+  'UEFA Champions League',
+  'UEFA Europa League',
+  'UEFA Europa Conference League'
+]
+
+export const generateLeagueOptions = () => {
+  let options = '';
+  allowed_leagues.forEach((league, index) => {
+    options += `${index + 1}. ${league}\n`;
+  });
+  return options;
+};
