a865aa3e668b0562d84cd289e1273886bfa1c13f
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-05-25 15:28:03 -0300

    dividing the functions and fixing everything

--- src/controller/update/addLeagues.ts
+++ src/controller/update/addLeagues.ts
@@ -27,72 +27,80 @@ export const addLeague = async (telegramId: string, name: string, msg: any, send
   };
 
   bot.sendMessage(msg.chat.id, 'What league do you wanna add?', opts);
-
-  async function getUserLeagues() {
-    try {
-      const user_leagues = await prisma.user.findUnique({
-        where: {
-          telegramId
-        },
-        select: {
-          leagues: true
-        }
-      });
-
-      await prisma.$disconnect();
-
-      return user_leagues?.leagues || [];
-    } catch (error) {
-      console.log(error);
-      return [];
-    }
-  }
-
-  async function checkIfLeagueAllowed(message: any, added_league: string): Promise<boolean> {
-    return new Promise<boolean>(async (resolve, reject) => {
-      try {
-        const leagues = await getUserLeagues();
-        if (leagues.includes(added_league)) {
-          await bot.sendMessage(message.chat.id, `${added_league} is already in your list`);
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      } catch (error) {
-        reject(error);
+}
+async function getUserLeagues(telegramId: any) {
+  try {
+    const user_leagues = await prisma.user.findUnique({
+      where: {
+        telegramId
+      },
+      select: {
+        leagues: true
       }
     });
+
+    await prisma.$disconnect();
+
+    return user_leagues?.leagues || [];
+  } catch (error) {
+    console.log('error');
+    return [];
   }
+}
 
-
-
-  async function updateLeagues(added_league: string) {
+async function checkIfLeagueAllowed(message: any, added_league: string, telegramId: any): Promise<boolean> {
+  return new Promise<boolean>(async (resolve, reject) => {
     try {
-      const user = await prisma.user.update({
-        where: {
-          telegramId: telegramId
-        },
-        data: {
-          leagues: {
-            push: added_league
-          },
-        }
-      });
-
+      const leagues = await getUserLeagues(telegramId);
+      if (leagues.includes(added_league)) {
+        await bot.sendMessage(message.chat.id, `${added_league} is already in your list`);
+        resolve(true);
+      } else {
+        resolve(false);
+      }
     } catch (error) {
-      console.log(error)
+      reject(error);
     }
+  });
+}
+
+async function updateLeagues(added_league: string, telegramId: any) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        telegramId: telegramId,
+      },
+    });
+
+    if (!user) {
+      console.log('User not found');
+      return;
+    }
+
+    await prisma.user.update({
+      where: {
+        telegramId: telegramId,
+      },
+      data: {
+        leagues: {
+          push: added_league,
+        },
+      },
+    });
+
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  async function stringLeagues(message: any, added_league: string) {
-    const leagues = await getUserLeagues();
-    const leaguesString = leagues.join(', ');
+async function stringLeagues(message: any, added_league: any, telegramId: any) {
+  const leagues = await getUserLeagues(telegramId);
+  const leaguesString = leagues.join(', ');
 
-    bot.sendMessage(message.chat.id, `Agora suas ligas são ${leaguesString}`);
-  }
+  bot.sendMessage(message.chat.id, `Agora suas ligas são ${leaguesString}`);
+}
 
-  bot.on('callback_query', async function onCallbackQuery(callbackQuery) {
-
+export const callback_league = async function onCallbackQuery(callbackQuery: any, telegramId: any) {
   const data = callbackQuery.data;
   const message = callbackQuery.message!;
 
@@ -100,113 +108,110 @@ export const addLeague = async (telegramId: string, name: string, msg: any, send
 
   if (data === 'pm') {
   added_league = 'Premier League';
-  const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+  const leagueAllowed = await checkIfLeagueAllowed(message, added_league, telegramId);
     if (!leagueAllowed) {
       await Promise.all([
-        bot.sendMessage(message.chat.id, 'Premier League was added to your list'),
-        updateLeagues(added_league),
-        stringLeagues(message, added_league)
+        await bot.sendMessage(message.chat.id, 'Premier League was added to your list'),
+        await updateLeagues(added_league, telegramId),
+        await stringLeagues(message, added_league, telegramId)
       ]);
     }
 
   } else if (data === 'll') {
     added_league = 'LaLiga';
-    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league, telegramId);
     if(!leagueAllowed){
       await Promise.all([
         await bot.sendMessage(message.chat.id, 'La Liga was added to your list'),
-        await updateLeagues(added_league),
-        stringLeagues(message, added_league)
+        await updateLeagues(added_league, telegramId),
+        await stringLeagues(message, added_league, telegramId)
       ])
     }
 
   } else if (data === 'sa') {
     added_league = 'Serie A';
-    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league, telegramId);
     if(!leagueAllowed){
       await Promise.all([
         await bot.sendMessage(message.chat.id, 'Serie A was added to your list'),
-        await updateLeagues(added_league),
-        stringLeagues(message, added_league)
+        await updateLeagues(added_league, telegramId),
+        await stringLeagues(message, added_league, telegramId)
       ])
     }
 
   } else if (data === 'bu') {
     added_league = 'Bundesliga';
-    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league, telegramId);
     if(!leagueAllowed){
       await Promise.all([
         await bot.sendMessage(message.chat.id, 'Bundesliga was added to your list'),
-        await updateLeagues(added_league),
-        stringLeagues(message, added_league),
+        await updateLeagues(added_league, telegramId),
+        await stringLeagues(message, added_league, telegramId),
       ])
     }
 
   } else if (data === 'l1') {
     added_league = 'Ligue 1';
-    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league, telegramId);
     if(!leagueAllowed){
       await Promise.all([
         await bot.sendMessage(message.chat.id, 'Ligue 1 was added to your list'),
-        await updateLeagues(added_league),
-        stringLeagues(message, added_league),
+        await updateLeagues(added_league, telegramId),
+        await stringLeagues(message, added_league, telegramId),
       ])
     }
 
   } else if (data === 'pl') {
     added_league = 'Primeira Liga';
-    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league, telegramId);
     if(!leagueAllowed){
       await Promise.all([
         await bot.sendMessage(message.chat.id, 'Primeira Liga was added to your list'),
-        await updateLeagues(added_league),
-        stringLeagues(message, added_league),
+        await updateLeagues(added_league, telegramId),
+        await stringLeagues(message, added_league, telegramId),
       ])
     }
 
   } else if (data === 'er') {
     added_league = 'Eredivisie';
-    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league, telegramId);
     if(!leagueAllowed){
       await Promise.all([
         await bot.sendMessage(message.chat.id, 'Eredivisie was added to your list'),
-        await updateLeagues(added_league),
-        stringLeagues(message, added_league),
+        await updateLeagues(added_league, telegramId),
+        await stringLeagues(message, added_league, telegramId),
       ])
     }
 
   } else if (data === 'ch1') {
     added_league = 'UEFA Champions League';
-    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league, telegramId);
     if(!leagueAllowed){
       await Promise.all([
         await bot.sendMessage(message.chat.id, 'UEFA Champions League was added to your list'),
-        await updateLeagues(added_league),
-        stringLeagues(message, added_league),
+        await updateLeagues(added_league, telegramId),
+        await stringLeagues(message, added_league, telegramId),
       ])
     }
 
   } else if (data === 'el') {
     added_league = 'UEFA Europa League';
-    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league, telegramId);
     if(!leagueAllowed){
       await Promise.all([
         await bot.sendMessage(message.chat.id, 'UEFA Europa League League was added to your list'),
-        await updateLeagues(added_league),
-        stringLeagues(message, added_league),
+        await updateLeagues(added_league, telegramId),
+        await stringLeagues(message, added_league, telegramId),
       ])
     }
   } else if (data === 'col') {
     added_league = 'UEFA Europa Conference League';
-    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league, telegramId);
     if(!leagueAllowed){
       await Promise.all([
         await bot.sendMessage(message.chat.id, 'UEFA Europa Conference League was added to your list'),
-        await updateLeagues(added_league),
-        stringLeagues(message, added_league),
+        await updateLeagues(added_league, telegramId),
+        await stringLeagues(message, added_league, telegramId),
       ])
-    }}
-  }
-)
-  return
+  }}
 }
