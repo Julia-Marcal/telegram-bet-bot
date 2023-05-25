3ad67bba0d65c2475d551242c222655edf4d1ae9
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-05-25 11:54:31 -0300

    fixing double message

--- src/controller/update/addLeagues.ts
+++ src/controller/update/addLeagues.ts
@@ -6,22 +6,24 @@ export const addLeague = async (telegramId: string, name: string, msg: any, send
   const isUserRegistered = await userCheck(telegramId);
   if (!isUserRegistered) return sendMessage('Se registre primeiro');
 
+   const leagues = [
+    { text: 'Premier League', value: 'Premier League', callback_data: 'pm' },
+    { text: 'LaLiga', value: 'LaLiga', callback_data: 'll' },
+    { text: 'Serie A', value: 'Serie A', callback_data: 'sa' },
+    { text: 'Bundesliga', value: 'Bundesliga', callback_data: 'bu' },
+    { text: 'Ligue 1', value: 'Ligue 1', callback_data: 'l1' },
+    { text: 'Primeira Liga', value: 'Primeira Liga', callback_data: 'pl' },
+    { text: 'Eredivisie', value: 'Eredivisie', callback_data: 'er' },
+    { text: 'UEFA Champions League', value: 'UEFA Champions League', callback_data: 'chl' },
+    { text: 'UEFA Europa League', value: 'UEFA Europa League', callback_data: 'el' },
+    { text: 'UEFA Europa Conference League', value: 'UEFA Europa Conference League', callback_data: 'col' },
+  ];
+
   const opts = {
     reply_to_message_id: msg.message_id,
     reply_markup: {
-      inline_keyboard: [
-        [{ text: 'Premier League', callback_data: 'pm' }],
-        [{ text: 'LaLiga', callback_data: 'll' }],
-        [{ text: 'Serie A', callback_data: 'sa' }],
-        [{ text: 'Bundesliga', callback_data: 'bu' }],
-        [{ text: 'Ligue 1', callback_data: 'l1' }],
-        [{ text: 'Primeira Liga', callback_data: 'pl' }],
-        [{ text: 'Eredivisie', callback_data: 'er' }],
-        [{ text: 'UEFA Champions League', callback_data: 'chl' }],
-        [{ text: 'UEFA Europa League', callback_data: 'el' }],
-        [{ text: 'UEFA Europa Conference League', callback_data: 'col' }]
-      ]
-    }
+      inline_keyboard: leagues.map((league) => [{ text: league.text, callback_data: league.callback_data }]),
+    },
   };
 
   bot.sendMessage(msg.chat.id, 'What league do you wanna add?', opts);
@@ -46,22 +48,23 @@ export const addLeague = async (telegramId: string, name: string, msg: any, send
     }
   }
 
-  function checkIfLeagueAllowed(message: any, added_league: string): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      getUserLeagues()
-        .then((leagues: string[]) => {
-          if (leagues.includes(added_league)) {
-            return bot.sendMessage(message.chat.id, `${added_league} is already in your list`);
-          }
-        })
-        .then(() => {
-          resolve();
-        })
-        .catch((error) => {
-          reject(error);
-        });
+  async function checkIfLeagueAllowed(message: any, added_league: string): Promise<boolean> {
+    return new Promise<boolean>(async (resolve, reject) => {
+      try {
+        const leagues = await getUserLeagues();
+        if (leagues.includes(added_league)) {
+          await bot.sendMessage(message.chat.id, `${added_league} is already in your list`);
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      } catch (error) {
+        reject(error);
+      }
     });
   }
+
+
 
   async function updateLeagues(added_league: string) {
     try {
@@ -85,7 +88,7 @@ export const addLeague = async (telegramId: string, name: string, msg: any, send
     const leagues = await getUserLeagues();
     const leaguesString = leagues.join(', ');
 
-    bot.sendMessage(message.chat.id, `Agora suas ligas são ${leaguesString}, e agora foi adiconado ${added_league}`);
+    bot.sendMessage(message.chat.id, `Agora suas ligas são ${leaguesString}`);
   }
 
   bot.on('callback_query', async function onCallbackQuery(callbackQuery) {
@@ -96,66 +99,114 @@ export const addLeague = async (telegramId: string, name: string, msg: any, send
   let added_league: string;
 
   if (data === 'pm') {
-    added_league = 'Premier League';
-    await checkIfLeagueAllowed(message, added_league);
-    await bot.sendMessage(message.chat.id, 'Premier League was added to your list');
-    await updateLeagues(added_league);
-    stringLeagues(message, added_league);
+  added_league = 'Premier League';
+  const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    if (!leagueAllowed) {
+      await Promise.all([
+        bot.sendMessage(message.chat.id, 'Premier League was added to your list'),
+        updateLeagues(added_league),
+        stringLeagues(message, added_league)
+      ]);
+    }
 
   } else if (data === 'll') {
     added_league = 'LaLiga';
-    await checkIfLeagueAllowed(message, added_league);
-    await bot.sendMessage(message.chat.id, 'La Liga was added to your list');
-    await updateLeagues(added_league);
-    stringLeagues(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    if(!leagueAllowed){
+      await Promise.all([
+        await bot.sendMessage(message.chat.id, 'La Liga was added to your list'),
+        await updateLeagues(added_league),
+        stringLeagues(message, added_league)
+      ])
+    }
 
   } else if (data === 'sa') {
     added_league = 'Serie A';
-    await checkIfLeagueAllowed(message, added_league);
-    await bot.sendMessage(message.chat.id, 'Serie A was added to your list');
-    await updateLeagues(added_league);
-    stringLeagues(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    if(!leagueAllowed){
+      await Promise.all([
+        await bot.sendMessage(message.chat.id, 'Serie A was added to your list'),
+        await updateLeagues(added_league),
+        stringLeagues(message, added_league)
+      ])
+    }
 
   } else if (data === 'bu') {
     added_league = 'Bundesliga';
-    await checkIfLeagueAllowed(message, added_league);
-    await bot.sendMessage(message.chat.id, 'Bundesliga was added to your list');
-    await updateLeagues(added_league);
-    stringLeagues(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    if(!leagueAllowed){
+      await Promise.all([
+        await bot.sendMessage(message.chat.id, 'Bundesliga was added to your list'),
+        await updateLeagues(added_league),
+        stringLeagues(message, added_league),
+      ])
+    }
 
   } else if (data === 'l1') {
     added_league = 'Ligue 1';
-    await bot.sendMessage(message.chat.id, 'Ligue 1 was added to your list');
-    await updateLeagues(added_league);
-    stringLeagues(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    if(!leagueAllowed){
+      await Promise.all([
+        await bot.sendMessage(message.chat.id, 'Ligue 1 was added to your list'),
+        await updateLeagues(added_league),
+        stringLeagues(message, added_league),
+      ])
+    }
 
   } else if (data === 'pl') {
     added_league = 'Primeira Liga';
-    await checkIfLeagueAllowed(message, added_league);
-    await bot.sendMessage(message.chat.id, 'Primeira Liga was added to your list');
-    await updateLeagues(added_league);
-    stringLeagues(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    if(!leagueAllowed){
+      await Promise.all([
+        await bot.sendMessage(message.chat.id, 'Primeira Liga was added to your list'),
+        await updateLeagues(added_league),
+        stringLeagues(message, added_league),
+      ])
+    }
 
   } else if (data === 'er') {
     added_league = 'Eredivisie';
-    await checkIfLeagueAllowed(message, added_league);
-    await bot.sendMessage(message.chat.id, 'Eredivisie was added to your list');
-    await updateLeagues(added_league);
-    stringLeagues(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    if(!leagueAllowed){
+      await Promise.all([
+        await bot.sendMessage(message.chat.id, 'Eredivisie was added to your list'),
+        await updateLeagues(added_league),
+        stringLeagues(message, added_league),
+      ])
+    }
 
   } else if (data === 'ch1') {
     added_league = 'UEFA Champions League';
-    await checkIfLeagueAllowed(message, added_league);
-    await bot.sendMessage(message.chat.id, 'UEFA Champions League was added to your list');
-    await updateLeagues(added_league);
-    stringLeagues(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    if(!leagueAllowed){
+      await Promise.all([
+        await bot.sendMessage(message.chat.id, 'UEFA Champions League was added to your list'),
+        await updateLeagues(added_league),
+        stringLeagues(message, added_league),
+      ])
+    }
 
   } else if (data === 'el') {
     added_league = 'UEFA Europa League';
-    await checkIfLeagueAllowed(message, added_league)
-    await bot.sendMessage(message.chat.id, 'UEFA Europa Conference League was added to your list');
-    await updateLeagues(added_league);
-    stringLeagues(message, added_league);
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    if(!leagueAllowed){
+      await Promise.all([
+        await bot.sendMessage(message.chat.id, 'UEFA Europa League League was added to your list'),
+        await updateLeagues(added_league),
+        stringLeagues(message, added_league),
+      ])
     }
+  } else if (data === 'el') {
+    added_league = 'UEFA Europa Conference League';
+    const leagueAllowed = await checkIfLeagueAllowed(message, added_league);
+    if(!leagueAllowed){
+      await Promise.all([
+        await bot.sendMessage(message.chat.id, 'UEFA Europa Conference League was added to your list'),
+        await updateLeagues(added_league),
+        stringLeagues(message, added_league),
+      ])
+    }}
   }
-)}
+)
+  return
+}
